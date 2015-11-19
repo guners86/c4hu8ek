db7358345bce90b17bf51f522f847a34e5c33e51
Damian Arturo Garces Paez <guners86@hotmail.com>
2015-11-19 17:54:16 +0000

    Se deja el metodo authenticate en application_controller

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -19,14 +19,5 @@ class Api::V1::PinsController < ApplicationController
     def pin_params
       params.require(:pin).permit(:title, :image_url)
     end
-    
-    def authenticate
-      user = User.find_by(email: request.headers['HTTP_X_USER_EMAIL'])
-      acces = user ? request.headers['X-API-TOKEN'] == user.api_token : false
-      
-      unless acces
-        render json: { errors: "Acceso denegado" }, status: 401
-      end
-    end
      
 end
